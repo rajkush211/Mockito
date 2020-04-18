0ec83ca3d5562cbb1ee74aaaec2b0611c2476c8d
rajkush211 <rajkush211.rk@gmail.com>
2020-04-18 10:34:15 +0530

    ADDED : Code to test class particular class using Mockito for unit testing

---
+++ src/main/java/com/bridgelabz/mockito/Customer.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.mockito;
+
+public class Customer {
+
+    public final String fName;
+    public final String lName;
+
+    public Customer(String fName, String lName) {
+        this.fName = fName;
+        this.lName = lName;
+    }
+}
---
+++ src/main/java/com/bridgelabz/mockito/CustomerReader.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.mockito;
+
+public class CustomerReader {
+    private EntityManager entityManager;
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager=entityManager;
+    }
+
+    public String findFullName(long id) {
+        Customer customer=  this.entityManager.find(Customer.class,id);
+        return customer.fName+" "+customer.lName;
+    }
+}
